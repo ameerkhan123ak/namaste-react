f03e8dc57d7e46912cc23e9940739591d6bf2de1
ameerkhan123ak <ameerkhan.ee20@bitsathy.ac.in>
2023-09-09 17:27:50 +0530

    updated 09/09-05:27

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const HeadingComponent =  (
+const HeadingComponent = () =>  (
     <h1 id='heading'>This is Heading</h1>
 )
 
-const Container = (props) => (
+const Component = () => (
     <div id='container'>
-        <h1>This is heading inside container</h1>
+        <h1>This is heading</h1>
     </div>
 )
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Container color='red' />)
+root.render(<Component />)
